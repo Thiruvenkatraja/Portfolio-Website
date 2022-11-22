b0d23dfd872acb1a2c8c6a171ab5e7f3d08ef816
Thiruvenkatraja <venkat.tv.raja@gmail.com>
2022-11-23 01:02:01 +0530

    stable

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,60 +1,100 @@
-import React from 'react';
-import './Contact.css';
-import emailjs from '@emailjs/browser';
-import { useRef,useState } from 'react';
-import {themeContext} from '../../Context'; 
-import {useContext} from 'react';
+import React from "react";
+import "./Contact.css";
+import emailjs from "@emailjs/browser";
+import { useRef, useState } from "react";
+import { themeContext } from "../../Context";
+import { useContext } from "react";
 
 export default function Contact() {
-  const theme=useContext(themeContext);
+  const theme = useContext(themeContext);
   const darkMode = theme.state.darkMode;
-    const form = useRef();
-    
-   
+  const form = useRef();
+
   const sendEmail = (event) => {
     event.preventDefault();
 
-    emailjs.sendForm('service_0vwmt3l', 'template_da6ixew', form.current, 'YiYaitATSo9eZOGLT')
-      .then((result) => {
+    emailjs
+      .sendForm(
+        "service_0vwmt3l",
+        "template_da6ixew",
+        form.current,
+        "YiYaitATSo9eZOGLT"
+      )
+      .then(
+        (result) => {
           console.log(result.text);
           setdone(true);
           form.reset();
-      }, (error) => {
+        },
+        (error) => {
           console.log(error.text);
-      });
-      setname('');
-      setemail('');
-      setmobile('');
-      setmessage('');
+        }
+      );
+    setname("");
+    setemail("");
+    setmobile("");
+    setmessage("");
+  };
+  const [name, setname] = useState("");
+  const [email, setemail] = useState("");
+  const [mobile, setmobile] = useState("");
+  const [message, setmessage] = useState("");
 
-    };
-    const[name,setname]=useState('');
-    const[email,setemail]=useState('');
-    const[mobile,setmobile]=useState('');
-    const[message,setmessage]=useState('');
+  const [done, setdone] = useState(false);
 
-    const[done, setdone] = useState(false);
-  
   return (
-    <div className='contact-form' id='Contact'>
-        <div className="w-left">
-            <div className="awesome">
-                <span style={{color:darkMode?'white':''}}>Get in Touch</span>
-                <span>Contact Me</span>
-                <div className="blur s-blur1" style={{background:'#ABF1FF94'}}></div>
-            </div>
+    <div className="contact-form" id="Contact">
+      <div className="w-left">
+        <div className="awesome">
+          <span style={{ color: darkMode ? "white" : "" }}>Get in Touch</span>
+          <span>Contact Me</span>
+          <div
+            className="blur s-blur1"
+            style={{ background: "#ABF1FF94" }}
+          ></div>
         </div>
-        <div className="c-right">
-            <form ref={form} onSubmit={sendEmail}>
-                <input type="text" name='user_name' className='user' placeholder='Name' onChange={event=>setname(event.target.value)} value={name}/>
-                <input type="email" name='user_email' className='user' placeholder='Email' onChange={event=>setemail(event.target.value)} value={email} />
-                <input type="number" name='user_phone' className='user' placeholder='Mobile' onChange={event=>setmobile(event.target.value)} value={mobile} />
-                <textarea name="message" className="user" placeholder='Message' onChange={event=>setmessage(event.target.value)} value={message}/>
-                <input type="submit" value='Send' className="button c-btn" />
-                <span>{done && "Thanks for Contacting me"}</span>
-                <div className= 'blur c-blur1' style={{background:"var(--purple)"}}></div>
-            </form>
-        </div>
+      </div>
+      <div className="c-right">
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            name="user_name"
+            className="user"
+            placeholder="Name"
+            onChange={(event) => setname(event.target.value)}
+            value={name}
+          />
+          <input
+            type="email"
+            name="user_email"
+            className="user"
+            placeholder="Email"
+            onChange={(event) => setemail(event.target.value)}
+            value={email}
+          />
+          <input
+            type="number"
+            name="user_phone"
+            className="user"
+            placeholder="Mobile"
+            onChange={(event) => setmobile(event.target.value)}
+            value={mobile}
+          />
+          <textarea
+            name="message"
+            className="user"
+            placeholder="Message"
+            onChange={(event) => setmessage(event.target.value)}
+            value={message}
+          />
+          <input type="submit" value="Send" className="button c-btn" />
+          <span>{done && "Thanks for Contacting me"}</span>
+          <div
+            className="blur c-blur1"
+            style={{ background: "var(--purple)" }}
+          ></div>
+        </form>
+      </div>
     </div>
   );
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,44 +1,76 @@
 import React from "react";
 import "./Navbar.css";
 import "../../App.css";
-import Toggle from '../../Components/Toggle/Toggle'
+import Toggle from "../../Components/Toggle/Toggle";
 import { Link } from "react-scroll";
-import{useState} from 'react';
+import { useState } from "react";
 // import {FaTimes, FaBars} from 'react-icons/fa';
 const Navbar = () => {
   const [click, setClick] = useState(false);
 
   const handleClick = () => setClick(!click);
   return (
-    <div className="n-wrapper" id='Navbar'>
+    <div className="n-wrapper" id="Navbar">
       <div className="n-left">
         <div className="n-name">
           <h1 className="profile-name">Venkadesh</h1>
-          </div>
-       <Toggle/>
+        </div>
+        <Toggle />
       </div>
       <div className="n-right">
         <div className="n-list">
           <ul className={click ? "nav-menu active" : "nav-menu"}>
-            <Link spy={true} to='Navbar' smooth={true} activeClass='activeClass' >
-            <li>Home</li>
+            <Link
+              spy={true}
+              to="Navbar"
+              smooth={true}
+              activeClass="activeClass"
+            >
+              <li>Home</li>
             </Link>
-            <Link spy={true} to='Education' smooth={true} activeClass='activeClass' >
-            <li>Education</li></Link>
-            <Link spy={true} to="Experience" smooth={true} activeClass='activeClass' >
-            <li>Experience</li></Link>
-            <Link spy={true} to='Projects' smooth={true} activeClass='activeClass' >
-            <li>Projects</li></Link>
-            <Link spy={true} to='Testimonials' smooth={true} activeClass='activeClass' >
-            <li>Testimonials</li></Link>
-             </ul>
-             <div className="nav-icons" onClick={handleClick}>
-            <i className= { click ? " button fas fa-times" : " button fas fa-bars"}></i>
+            <Link
+              spy={true}
+              to="Education"
+              smooth={true}
+              activeClass="activeClass"
+            >
+              <li>Education</li>
+            </Link>
+            <Link
+              spy={true}
+              to="Experience"
+              smooth={true}
+              activeClass="activeClass"
+            >
+              <li>Experience</li>
+            </Link>
+            <Link
+              spy={true}
+              to="Projects"
+              smooth={true}
+              activeClass="activeClass"
+            >
+              <li>Projects</li>
+            </Link>
+            <Link
+              spy={true}
+              to="Testimonials"
+              smooth={true}
+              activeClass="activeClass"
+            >
+              <li>Testimonials</li>
+            </Link>
+          </ul>
+          <div className="nav-icons" onClick={handleClick}>
+            <i
+              className={click ? " button fas fa-times" : " button fas fa-bars"}
+            ></i>
           </div>
         </div>
-        
-        <Link spy={true} to='Contact' smooth={true} activeClass='activeClass' >
-        <button className="button n-button" >Contact</button></Link>
+
+        <Link spy={true} to="Contact" smooth={true} activeClass="activeClass">
+          <button className="button n-button">Contact</button>
+        </Link>
       </div>
     </div>
   );
